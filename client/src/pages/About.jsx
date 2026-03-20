0931c7c169dd780107b3d9716a639da9676d4bff
guniluk <guniluk@gmail.com>
2026-03-20 15:58:34 +0900

    complete about page and completely finish

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,19 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className=" flex flex-col gap-7 p-7 max-w-6xl mx-auto">
+      <h1 className="text-3xl text-shadow-slate-700 font-semibold">
+        About Young Estate
+      </h1>
+      <p className="text-1xl text-slate-600 ">
+        영하부동산은 신뢰를 바탕으로 고객에게 언제나 최선을 다합니다
+        <br />
+        오랜 전통은 없지만 해당 분야 전문성을 바탕으로 고객에게 언제나 정확하고
+        가장 적합한 부동산을 제공합니다
+        <br />
+        부동산 매매, 전세, 월세 등 모든 유형의 거래에 대해 안전하고 정확하게
+        계약이 체결될 수 있도록 지원합니다
+        <br />
+      </p>
+    </div>
+  );
 }
